5a6ba83e7dd79bc3f8e26f3a7190ea7f4b15728f
Abdullah-codes <abdullahshaikhneo@gmail.com>
2021-10-26 13:20:34 +0530

    change in bashscript

--- bash-automation.sh
+++ bash-automation.sh
@@ -22,7 +22,8 @@ else
   sudo apt install unzip
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
   unzip awscliv2.zip
-  sudo ./aws/install
+  ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
+  aws --version
   echo "aws cli is was not installed and now installed successfully"
 fi
 
