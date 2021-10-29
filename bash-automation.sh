55bab2dbc7735346cb22b8f0fb367e91651f9648
Abdullah-codes <abdullahshaikhneo@gmail.com>
2021-10-29 12:22:42 +0530

    2 changes in jenkins and bashscript

--- bash-automation.sh
+++ bash-automation.sh
@@ -36,7 +36,7 @@ fi
 
 # configuring kubectl to use kubernetes cluster
 
-$HOME/bin/aws eks --region us-east-2 update-kubeconfig --name education-eks-DBNYMYb6
+# $HOME/bin/aws eks --region us-east-2 update-kubeconfig --name education-eks-DBNYMYb6
 
 #giving aws command path to config file (beacuse we installed aws cli in jenkins user) 
-sed -i '$s/aws/\/var\/lib\/jenkins\/bin\/aws/g' $HOME/.kube/config 
+# sed -i '$s/aws/\/var\/lib\/jenkins\/bin\/aws/g' $HOME/.kube/config 
